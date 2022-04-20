0b440c8fd72651742ba42e713a61ea787477a42a
Technology App <soportetechnologyapp@gmail.com>
2022-04-20 14:24:18 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomerService } from './services/customer.service';
+import { Customer } from './models/customer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'testSodimac';
+export class AppComponent implements OnInit {
+  formCustomer!: FormGroup;
+
+  constructor(private customerService: CustomerService) {}
+
+  //TODO Funcion que inicializa el formulario
+  private validationForm(): void {
+    this.formCustomer = new FormGroup({
+      nameCustomer: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(50),
+        Validators.minLength(3),
+      ]),
+    });
+  }
+
+  ngOnInit(): void {
+    this.validationForm();
+  }
+
+  addCustomer() {
+    //TODO Se hace el llamado al servicio de Customer, el cual comparte un la información con otros componentes
+    this.customerService.listCustomer.emit(this.setDataForm());
+    this.formCustomer.reset();
+  }
+
+  //TODO Se da formato a la data que viene del formulario
+  setDataForm(): Customer {
+    return {
+      nameCustomer: this.formCustomer.get('nameCustomer')?.value,
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ListCustomerComponent } from './component/list-customer/list-customer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ListCustomerComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
